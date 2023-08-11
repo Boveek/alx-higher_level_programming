5d8e33ce06f7b8a87f8cec7ac18345f2113f1986
Ololade Victor <oluwabori2014@gmail.com>
2023-08-10 22:57:34 -0700

    python while,if-else,loops

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for alphabeth in range(97, 123):
+    if chr(alphabeth) != 'q' and chr(alphabeth) != 'e':
+        print("{}".format(chr(alphabeth)), end="")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains python project for if-else, loop and function
