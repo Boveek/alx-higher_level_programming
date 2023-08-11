048c2c68699610985ab0119827ac3b8e0c840bae
Ololade Victor <oluwabori2014@gmail.com>
2023-08-11 00:00:01 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num1 = abs(number)%10
+num1 = abs(number) % 10
 if number < 0:
     num1 = -num1
 if num1 > 5:
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabeth in range (97, 123):
+for alphabeth in range(97, 123):
     print("{}".format(chr(alphabeth)), end="")
