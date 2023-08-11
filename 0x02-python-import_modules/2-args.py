4711150a16823c8f941a8afbbc9f1d4e5a951005
Ololade Victor <oluwabori2014@gmail.com>
2023-08-11 15:10:40 -0700

    Python import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-if argc == 0:
-    print("0 arguments")
-elif argc == 1:
-    print("1 argument:")
-    print(f"{argc}: {sys.argv[1]}")
-elif argc > 1:
-    print(f"{argc} arguments:")
-    for i in range(1, argc + 1):
-        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments")
+    elif argc == 1:
+        print("1 argument:")
+        print(f"{argc}: {sys.argv[1]}")
+    elif argc > 1:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
