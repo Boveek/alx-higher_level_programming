9b6a1aa904805d9b3cf861334520f8ccf6a872cb
Ololade Victor <oluwabori2014@gmail.com>
2023-08-14 15:15:11 -0700

    Python data structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     num = len(my_list)
-    for i in range(0, num):
+    for i in range(num):
         print("{}".format(my_list[i]))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     num = len(my_list)
-    for i in range(num - 1, -1, -1):
+    my_list.reverse()
+    for i in range(num):
         print("{}".format(my_list[i]))
