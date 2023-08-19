bc964236796d44b195eb03f16dd06ba467407661
Ololade Victor <oluwabori2014@gmail.com>
2023-08-18 17:39:51 -0700

    Python more data structure

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_value = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    num = len(roman_string)
+    summ = 0
+    for n in range(num - 1):
+        if ((roman_value[roman_string[n]]) < (roman_value[roman_string[n + 1]])):
+            summ -= roman_value[roman_string[n]]
+        else:
+            summ += roman_value[roman_string[n]]
+    summ += roman_value[roman_string[num - 1]]
+    return summ
