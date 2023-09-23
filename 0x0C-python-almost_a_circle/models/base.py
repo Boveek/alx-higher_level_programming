6fcbab7e7efd67a7807e5c80499020a3d2beb9a1
Ololade Victor <oluwabori2014@gmail.com>
2023-09-23 01:43:18 +0000

    changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,38 +5,45 @@ import csv
 
 
 class Base:
+	""" A representation of the base of our OOP hierarchy"""
+
 	__nb_objects = 0
 	def __init__(self, id=None):
+		""" Instancialize constructor"""
 		if id == None:
 			Base.__nb_objects += 1
 			self.id = Base.__nb_objects
 		else:
 			self.id = id
+
 	@staticmethod
 	def to_json_string(list_dictionaries):
+		""" Jsonifies a dictionary so it's quite rightly and longer"""
 		if list_dictionaries is None or list_dictionaries == []:
-		return []
-      return json.dumps(list_dictionaries)
+			return []
+		return json.dumps(list_dictionaries)
 
 	@classmethod
 	def save_to_file(cls, list_objs):
+		""" Saves jsonified object to file"""
 		filename = cls.__name__ + ".json"
 		with open(filename, "w") as jsonfile:
 			if list_objs is None:
 				jsonfile.write("[]")
-
-            		else:
+			else:
 				list_dicts = [o.to_dictionary() for o in list_objs]
 				jsonfile.write(Base.to_json_string(list_dicts))
 
 	@staticmethod
 	def from_json_string(json_string):
+		""" Unjsonifies a dictionary"""
 		if json_string is None or json_string == "[]":
 			return []
 		return json.loads(json_string)
 
 	@classmethod
 	def create(cls, **dictionary):
+		""" Loads instance from dictionary"""
 		if dictionary and dictionary != {}:
 			if cls.__name__ == "Rectangle":
 				new = cls(1, 1)
@@ -47,6 +54,7 @@ class Base:
 
 	@classmethod
 	def load_from_file(cls):
+		""" Loads object to csv file"""
 		filename = str(cls.__name__) + ".json"
 		try:
 			with open(filename, "r") as jsonfile:
@@ -57,21 +65,23 @@ class Base:
 
 	@classmethod
 	def save_to_file_csv(cls, list_objs):
+		""" Saves object to csv file"""
 		filename = cls.__name__ + ".csv"
 		with open(filename, "w", newline="") as csvfile:
 			if list_objs is None or list_objs == []:
 				csvfile.write("[]")
-		else:
-			if cls.__name__ == "Rectangle":
-				fieldnames = ["id", "width", "height", "x", "y"]
 			else:
-				fieldnames = ["id", "size", "x", "y"]
-				writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-				for obj in list_objs:
-					writer.writerow(obj.to_dictionary())
+				if cls.__name__ == "Rectangle":
+					fieldnames = ["id", "width", "height", "x", "y"]
+				else:
+					fieldnames = ["id", "size", "x", "y"]
+					writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+					for obj in list_objs:
+						writer.writerow(obj.to_dictionary())
 
 	@classmethod
 	def load_from_file_csv(cls):
+		""" Loads object to csv file"""
 		filename = cls.__name__ + ".csv"
 		try:
 			with open(filename, "r", newline="") as csvfile:
@@ -84,4 +94,4 @@ class Base:
 						for d in list_dicts]
 					return [cls.create(**d) for d in list_dicts]
 		except IOError:
-			return []
+				return []
